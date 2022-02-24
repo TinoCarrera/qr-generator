7672548850f93443e40559531d7b12390b2ad436
Florentino Carrera <florentinocarrera44@gmail.com>
2022-02-24 00:22:39 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import qrcode
+import sys
+
+def main():
+  if len(sys.argv) < 3:
+    sys.exit('Please enter a valid arguments. Example: python main.py "url" "file_name"')
+  else:
+    url = sys.argv[1]
+    file_name = sys.argv[2]
+  qr = qrcode.QRCode(version=1, border=0)
+  qr.add_data(url)
+  qr.make(fit=True)
+  img = qr.make_image(fill_color="white", back_color="transparent")
+  img.save('{}.png'.format(file_name))
+
+if __name__=="__main__":
+  main()
